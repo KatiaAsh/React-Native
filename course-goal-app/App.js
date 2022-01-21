7a00a5416a7d317186b68b11c949358aaa576e3d
katiaassh <katia.ashkar7@gmail.com>
2022-01-21 17:22:22 +0200

    Change from ScrollView to FlatList Fix #16

--- course-goal-app/App.js
+++ course-goal-app/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
@@ -12,31 +12,40 @@ export default function App() {
 
   const addGoalHandler = () => {
     //...: spread operator, JS featre which takes an existing array and course goals is an array
-    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+    setCourseGoals(currentGoals => [...currentGoals, {id: Math.random().toString(), value: enteredGoal}]);
   };
 
   return (
-    <ScrollView>
-      <View style={styles.screen}>
-        <View style={styles.inputContainer}>
-          <TextInput
-            placeholder="Course Goal"
-            style={styles.input}
-            //without parentheses, cuz this would execute this function when this code gets parsed
-            //when the UI gets rendered for the first time. you don't want to execute this immediately.
-            onChangeText={goalInputHandler}
-            value={enteredGoal}
-          />
-          <Button title="ADD" onPress={addGoalHandler} />
-        </View>
-        <ScrollView>
-          {coursGoals.map((goal) => (
-            <View key={goal} style={styles.listItem}>
-              <Text>{goal}</Text>
-            </View>))}
-        </ScrollView>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          //without parentheses, cuz this would execute this function when this code gets parsed
+          //when the UI gets rendered for the first time. you don't want to execute this immediately.
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
-    </ScrollView>
+      {/*FlatList: has 2 important properties
+      The first property is the data property where you point at your input data, and it should point at array.
+      The second property is render ite,, that takes a function which is called for every item in your data to render a list item */}
+      <FlatList 
+      // The key extractor property which takes a function that tells FlatList how to extract your key and by default
+      keyExtractor={(item, index) => item.id}
+        data={coursGoals} 
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )} />
+
+      {/* {coursGoals.map((goal) => (
+          <View key={goal} style={styles.listItem}>
+            <Text>{goal}</Text>
+          </View>))} */}
+    </View>
   );
 }
 
