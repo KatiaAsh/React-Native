c9dff418ca9b5861cd425252cca1d358d23d15f0
katiaassh <katia.ashkar7@gmail.com>
2022-04-12 10:20:52 +0300

    Create components folder and 2 files for goal item and goal input remove from app.js everything for the goal item and mopve it to GoalItem.js Fix #18

--- course-goal-app/App.js
+++ course-goal-app/App.js
@@ -1,76 +1,78 @@
-import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
+import { useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  Button,
+  TextInput,
+  FlatList,
+} from 'react-native';
+
+import GoalItem from './components/GoalItem';
 
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState('');
-  const [coursGoals, setCourseGoals] = useState([]);
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
 
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
-  };
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
 
-  const addGoalHandler = () => {
-    //...: spread operator, JS featre which takes an existing array and course goals is an array
-    setCourseGoals(currentGoals => [...currentGoals, {id: Math.random().toString(), value: enteredGoal}]);
-  };
+  function addGoalHandler() {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      { text: enteredGoalText, id: Math.random().toString() },
+    ]);
+  }
 
   return (
-    <View style={styles.screen}>
+    <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
         <TextInput
-          placeholder="Course Goal"
-          style={styles.input}
-          //without parentheses, cuz this would execute this function when this code gets parsed
-          //when the UI gets rendered for the first time. you don't want to execute this immediately.
+          style={styles.textInput}
+          placeholder="Your course goal!"
           onChangeText={goalInputHandler}
-          value={enteredGoal}
         />
-        <Button title="ADD" onPress={addGoalHandler} />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
-      {/*FlatList: has 2 important properties
-      The first property is the data property where you point at your input data, and it should point at array.
-      The second property is render ite,, that takes a function which is called for every item in your data to render a list item */}
-      <FlatList 
-      // The key extractor property which takes a function that tells FlatList how to extract your key and by default
-      keyExtractor={(item, index) => item.id}
-        data={coursGoals} 
-        renderItem={itemData => (
-          <View style={styles.listItem}>
-            <Text>{itemData.item.value}</Text>
-          </View>
-        )} />
-
-      {/* {coursGoals.map((goal) => (
-          <View key={goal} style={styles.listItem}>
-            <Text>{goal}</Text>
-          </View>))} */}
+      <View style={styles.goalsContainer}>
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return <GoalItem text={itemData.item.text} />;
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+          alwaysBounceVertical={false}
+        />
+      </View>
     </View>
   );
 }
 
-//The argument are passing here ia a javascript object and in that object,
-//can now define the styles.
 const styles = StyleSheet.create({
-  screen: {
-    padding: 50
+  appContainer: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center'
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
   },
-  input: {
-    width: 200,
-    borderBottomColor:
-      'black',
-    borderBottomWidth: 1
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8,
   },
-  listItem: {
-    padding: 10,
-    marginVertical: 10,
-    backgroundColor: '#ccc',
-    borderColor: 'black',
-    borderWidth: 1
-  },
+  goalsContainer: {
+    flex: 5,
+  }
 });
